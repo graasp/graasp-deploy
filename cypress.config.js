db76851ab246c1ac5cbf9c36343b0d772542d60d
Víctor González <36533965+victorggonzalez@users.noreply.github.com>
2022-08-28 00:51:43 +0200

    feat: init yarn and install cypress

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    // implement node event listeners here
+    setupNodeEvents(on, config) {
+      // modify config values
+      config.video = false;
+      config.chromeWebSecurity = false;
+      config.experimentalSessionAndOrigin = true;
+
+      // return the updated config object
+      return config;
+    },
+  },
+  env: {
+    TEST_EMAIL: process.env.TEST_EMAIL,
+    TEST_PASSWORD: process.env.TEST_PASSWORD,
+    REACT_APP_GRAASP_PERFORM_HOST_STAGE: process.env.REACT_APP_GRAASP_PERFORM_HOST_STAGE,
+    REACT_APP_GRAASP_COMPOSE_HOST_STAGE: process.env.REACT_APP_GRAASP_COMPOSE_HOST_STAGE,
+    REACT_APP_GRAASP_EXPLORE_HOST_STAGE: process.env.REACT_APP_GRAASP_EXPLORE_HOST_STAGE,
+    REACT_APP_AUTHENTICATION_HOST_STAGE: process.env.REACT_APP_AUTHENTICATION_HOST_STAGE,
+  }
+});
