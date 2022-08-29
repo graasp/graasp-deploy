f7f9b4ef3923efdff37e6eafcf6fc0d8c2d56c47
Víctor González <36533965+victorggonzalez@users.noreply.github.com>
2022-08-29 12:38:05 +0200

    style: userstory test

--- cypress/e2e/UserStory1.cy.js
+++ cypress/e2e/UserStory1.cy.js
@@ -1,34 +1,27 @@
 // UserStory1 Test: See user profile and verify member name and thumbnail
-describe(
-  "See profile Test",
-  {
-    retries: {
-      runMode: 2,
-      openMode: 2,
-    },
-  },
-  () => {
-    // Perform Login before UserStory test
-    before(() => {
-      cy.visit(Cypress.env("REACT_APP_GRAASP_COMPOSE_HOST_STAGE"));
-      cy.url().should(
-        "be.equal",
-        Cypress.env("REACT_APP_AUTHENTICATION_HOST_STAGE") + "/signin"
-      );
-      cy.login(Cypress.env("TEST_EMAIL"), Cypress.env("TEST_PASSWORD")); // custom command that hanldes signIn wth password
-    });
+describe("See profile Test", () => {
+  // Perform Login before UserStory test
+  before(() => {
+    cy.visit(Cypress.env("REACT_APP_GRAASP_COMPOSE_HOST_STAGE"));
+    cy.url().should(
+      "be.equal",
+      Cypress.env("REACT_APP_AUTHENTICATION_HOST_STAGE") + "/signin"
+    );
+    cy.login(Cypress.env("TEST_EMAIL"), Cypress.env("TEST_PASSWORD")); // custom command that hanldes signIn wth password
+  });
 
-    // Comment out the after() clause with the custom logout command
-    // in order to debug the application or write a partial test
-    after(() => {
-      cy.logout(); // custom logout command
-      cy.url().should("be.equal", "https://auth.stage.graasp.org/signin");
-    });
-    it("Navigate to profile", () => {
-      cy.get("#headerMemberMenuButton", {timeout: 10000}).click();
-      cy.get("#headerMemberMenuSeeProfileButton", {timeout: 10000}).click();
-      cy.get("#memberProfileMemberName", {timeout: 10000}).should("be.visible");
-      cy.get(".jss38 > :nth-child(1) > .MuiTypography-h5", {timeout: 10000}).should("be.visible");
-    });
-  }
-);
+  // Comment out the after() clause with the custom logout command
+  // in order to debug the application or write a partial test
+  after(() => {
+    cy.logout(); // custom logout command
+    cy.url().should("be.equal", "https://auth.stage.graasp.org/signin");
+  });
+  it("Navigate to profile", () => {
+    cy.get("#headerMemberMenuButton", { timeout: 10000 }).click();
+    cy.get("#headerMemberMenuSeeProfileButton", { timeout: 10000 }).click();
+    cy.get("#memberProfileMemberName", { timeout: 10000 }).should("be.visible");
+    cy.get(".jss38 > :nth-child(1) > .MuiTypography-h5", {
+      timeout: 10000,
+    }).should("be.visible");
+  });
+});
